dafa40ed4bc9ec9bd2d0f30c36a7ae6fd4231048
Chloe Saurat <sauratchloe@gmail.com>
2019-12-18 13:53:50 -0500

    HTML

---
+++ index.js
@@ -0,0 +1,23 @@
+const collapseBox = (anchor, box) => {
+  box.style.display = "none";
+  let openBox = false;
+  anchor.addEventListener("click", (event) => {
+    event.preventDefault();
+    // event.currentTarget.innerHTML = '';
+    if (openBox) {
+      event.currentTarget.innerHTML = '<i class="fas fa-chevron-right"></i>';
+      box.style.display = "none";
+      openBox = false;
+      // console.log('click false');
+    } else {
+      event.currentTarget.innerHTML = '<i class="fas fa-chevron-down"></i>';
+      box.style.display = "block";
+      openBox = true;
+      // console.log('click true');
+    }
+  });
+};
+
+const experienceAnchor = document.querySelector('a[href="#collapseExperience"]');
+const experienceBox = document.querySelector("#collapseExperience");
+collapseBox(experienceAnchor, experienceBox);
